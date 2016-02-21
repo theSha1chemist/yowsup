e54b970b1bca9214e14cd5520292b1ea4a6bd0e1
thesha1chemist <spmaltei@gmail.com>
2016-02-21 03:09:54 +0000

    working framework...

--- yowsup/demos/secbot/layer.py
+++ yowsup/demos/secbot/layer.py
@@ -1,4 +1,30 @@
 from yowsup.layers.interface                           import YowInterfaceLayer, ProtocolEntityCallback
+import modules
+import os
+
+#########################################################################
+# begin stolen code for listing possible functions in the modules directory. 
+# The idea is any new function put in the modules directory will get picked up here...
+#########################################################################
+def module_names(package):
+    folder = os.path.split(package.__file__)[0]
+    for name in os.listdir(folder):
+        if name.endswith(".py") and not name.startswith("__"):
+            yield name[:-3]
+
+
+def import_submodules(package):
+    names = list(module_names(package))
+    m = __import__(package.__name__, globals(), locals(), names, -1)
+    return dict((name, getattr(m, name)) for name in names)
+#########################################################################
+#  end stolen code 
+#########################################################################
+
+def possible_commands():
+    commands = import_submodules(modules)
+    return commands.keys()
+
 
 class EchoLayer(YowInterfaceLayer):
 
@@ -14,14 +40,37 @@ class EchoLayer(YowInterfaceLayer):
         self.toLower(messageProtocolEntity.ack())
         self.toLower(messageProtocolEntity.ack(True))
 
-
     @ProtocolEntityCallback("receipt")
     def onReceipt(self, entity):
         self.toLower(entity.ack())
 
     def onTextMessage(self,messageProtocolEntity):
-        # just print info
-        print("Echoing %s to %s" % (messageProtocolEntity.getBody(), messageProtocolEntity.getFrom(False)))
+        cmds = possible_commands()
+        message = messageProtocolEntity.getBody()
+
+        if message[0] == "!": 
+            command = message[1:].split(" ")
+            if command[0] in cmds:
+                query = ' '.join(command[1:])
+                print query
+                function = "modules.%s.%s('%s')" % (command[0], command[0], query) 
+                print function
+                try:
+                    a = eval(function)
+                    messageProtocolEntity.setBody(a)
+                except Exception, e:
+                    messageProtocolEntity.setBody(str(e))
+                    pass
+
+            else:
+                help = "Command not found. Try: \n!%s" % ('\n!'.join(cmds))
+                messageProtocolEntity.setBody(help)
+
+
+
+
+        print("Sending %s to %s" % (messageProtocolEntity.getBody(), messageProtocolEntity.getFrom(False)))
+        
 
     def onMediaMessage(self, messageProtocolEntity):
         # just print info
