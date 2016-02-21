8ff917c5b7f01289e97c26ae0018dc48973897aa
thesha1chemist <spmaltei@gmail.com>
2016-02-21 03:10:48 +0000

    removing old files

--- yowsup/modules/is_the_internet_on_fire.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-def isTheInternetOnFire():
-    r = requests.get('https://www.istheinternetonfire.com/status.txt')
-    return r.content
--- yowsup/modules/urban_dictionary.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-def urbanlookup(query):
-    r = requests.get('http://api.urbandictionary.com/v0/define?term=%s' % (query))
-    data = json.loads(r.content)
-    definition = data['list'][0]['definition']
-    return definition
